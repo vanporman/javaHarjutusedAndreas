104cf0563aca4d8080a0d001d73af4f73473fe00
vanporman <vanporman@gmail.com>
2016-11-11 00:19:48 +0200

    praktikum6 tyhi kaust

---
+++ src/praktikum6/KullJaKiri.java
@@ -0,0 +1,10 @@
+package praktikum6;
+
+public class KullJaKiri {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/praktikum6/LiisuT6mbamine.java
@@ -0,0 +1,10 @@
+package praktikum6;
+
+public class LiisuT6mbamine {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
