f806c5dfe6785fcf0d3d383e3f8c4805b40e64d4
Andreas Porman <Andreas Porman>
2016-11-11 17:45:59 +0200

    praktikum7 1 harjutus

---
+++ src/praktikum7/Arva2Ra.java
@@ -0,0 +1,24 @@
+package praktikum7;
+
+import praktikum6.Meetodid;
+
+public class Arva2Ra {
+
+	public static void main(String[] args) {
+		
+		int arvutiArv = Meetodid.suvalineArv(1, 100);
+		
+		while (true) {
+			int kasutajaArv = Meetodid.kasutajaSisestus("Palun sisesta arv ühest sajani", 1, 100);
+			if (kasutajaArv == arvutiArv) {
+				System.out.println("Arvasid ära!");
+				break;
+			} else if (kasutajaArv > arvutiArv) {
+				System.out.println("Arvuti mõeldud arv on väiksem");
+			} else {
+				System.out.println("Arvuti mõeldud arv on suurem");
+			} 
+		}
+	}
+
+}
